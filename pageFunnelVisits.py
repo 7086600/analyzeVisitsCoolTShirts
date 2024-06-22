6d642d373d45134470a6df2541231f024b71219b
Alexander <7086600@gmail.com>
2024-06-22 21:40:33 +0300

    Inspect the DataFrames

---
+++ pageFunnelVisits.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+visits = pd.read_csv('visits.csv', parse_dates=[1])
+cart = pd.read_csv('cart.csv', parse_dates=[1])
+checkout = pd.read_csv('checkout.csv', parse_dates=[1])
+purchase = pd.read_csv('purchase.csv', parse_dates=[1])
+
+#Inspect the DataFrames
+print(visits.head())
+print()
+print(cart.head())
+print()
+print(checkout.head())
+print()
+print(purchase.head())
