d672b643404ec8e8291bcddd6962acba7c89af82
Alexander <7086600@gmail.com>
2024-06-23 13:06:02 +0300

    count percentNullCartUsers, merge all data

--- pageFunnelVisits.py
+++ pageFunnelVisits.py
@@ -7,8 +7,10 @@ purchase = pd.read_csv('purchase.csv', parse_dates=[1])
 
 #Inspect the DataFrames
 print(visits.head())
+print(len(visits))
 print()
 print(cart.head())
+print(len(cart))
 print()
 print(checkout.head())
 print()
@@ -21,9 +23,30 @@ print(visitsAndCart)
 
 #find out the number of rows
 lenVisitsAndCart = len(visitsAndCart)
-print(f"The number of row is: {lenVisitsAndCart}")
+print(f"The count of users: {lenVisitsAndCart}")
 
 #count no items in cart users
-nullCartTimeRows = visitsAndCart[visitsAndCart["cart_time"].isnull()]
-lenNullCartTimeRows = len(nullCartTimeRows)
-print(f"The count of users without any items in carts are: {lenNullCartTimeRows}")
+nullCartUsers = visitsAndCart[visitsAndCart["cart_time"].isnull()]
+lenNullCartUsers = len(nullCartUsers)
+print(f"The count of users without any items in carts are: {lenNullCartUsers}")
+
+#percent of users who visited ended up not placing a t-shirt in their cart
+percentNullCartUsers = float(lenNullCartUsers) / lenVisitsAndCart * 100
+print(f"Percent of users who visited and not placing anything in their cart: {percentNullCartUsers}")
+print()
+
+#merge cart and checkout, count users
+cartAndCheckout = pd.merge(cart, checkout, how="left")
+print(cartAndCheckout)
+print(f"The count of users with anything in their cart: {len(cartAndCheckout)}")
+countNullCheckoutUsers = len(cartAndCheckout[cartAndCheckout["checkout_time"].isnull()])
+print(f"The count of users did not proceed to checkout: {countNullCheckoutUsers}")
+percentNullCheckoutUsers = float(countNullCheckoutUsers) / len(cartAndCheckout) * 100
+print(f"Percent of users who put items in their cart, but did not proceed to checkout: {percentNullCheckoutUsers:.2f}")
+print()
+
+#merge all data
+allData = visits.merge(cart, how="left") \
+    .merge(checkout, how="left") \
+    .merge(purchase, how="left")
+print(allData)
